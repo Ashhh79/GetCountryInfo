fadc627aca016b4f0a2008be8cf9747b2595b4fe
Ashhh79 <akash.kushwaha@somaiya.edu>
2022-03-08 00:26:36 +0530

    CountryInfo

--- index.js
+++ index.js
@@ -1,9 +1,174 @@
-const BASE_URL="https://restcountries.com/v3.1";
-const API_URL=BASE_URL+'/name/india';
+/*const BASE_URL="https://restcountries.com/v3.1/all";
+const API_URL=BASE_URL;
+
+const main_table = document.getElementById("main_table");
+
+
 getCountriesAll(API_URL);
 
 function getCountriesAll(url){
-    fetch(url).then(res=>res.json()).then(data=>{
-        console.log(data);
+    fetch(API_URL)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.results);
+            showData(data.results);
+        })
+        
+}
+
+
+function showData(data){
+    //main_table.innerHTML="";
+
+    data.forEach((countries)=>{
+        const {name,capital}=countries;
+        const CountryEL=document.createElement('div');
+        CountryEl.classList.add('info');
+        CountryEl.innerHTML=`
+            
+                <tr>
+                    <td>1.</td>
+                    <td>${name}</td>
+                    <td>${capital}</td>
+                    
+                </tr>
+                
+        `
+        main.appendChild(CountryEL);
     })
+
+}*/
+
+
+
+
+//functionto get country
+
+
+
+const CountriesElem=document.querySelector(".Countries");
+const dropDown=document.querySelector(".dropDown");
+const dropElem=document.querySelector(".drop");
+const region=document.querySelectorAll(".region");
+const regionName=document.getElementsByClassName("regionName");
+const search=document.querySelector(".search");
+const toggle=document.querySelector(".toggle");
+const moon=document.querySelector(".moon");
+
+
+
+async function getCountry(){
+    const url=await fetch("https://restcountries.com/v2/all");
+    const res=await url.json();
+    console.log(res);
+    res.forEach(element=>{
+        showCountry(element);
+    });
+}
+
+getCountry();
+
+function showCountry(data){
+    const country=document.createElement('div');
+    country.classList.add("country");
+    country.innerHTML=`
+            <div class="country-img">
+                <img src="${data.flag}" alt="" >
+            </div>
+            <div class="country-info">
+                <h3 class="countryName">${data.name}</h3>
+                <p><strong>population:</strong>${data.population}</p>
+                <p class="regionName"><strong>region:</strong>${data.region}</p>
+                <p><strong>Capital:</strong>${data.capital}</p>
+                <p><strong>population:</strong>1515487</p>
+            </div>
+    `;
+    CountriesElem.appendChild(country);
+    country.addEventListener("click",()=>{
+        showCountryDetail(data);
+    })
+}
+
+
+
+dropDown.addEventListener("click",()=>{
+    dropElem.classList.toggle("showDropDown");
+    
+})
+
+region.forEach(element=>{
+    element.addEventListener("click",()=>{
+        
+        Array.from(regionName).forEach(elem=>{
+            if(elem.innerText.includes(element.innerText)||element.innerText=="All"){
+                elem.parentElement.parentElement.style.display="grid";
+
+            }
+            else{
+                elem.parentElement.parentElement.style.display="none";
+            }
+
+        })
+    })
+})
+const countryName=document.getElementsByClassName("countryName");
+
+
+search.addEventListener("input",()=>{
+    //console.log(search.value.toLowerCase());
+    Array.from(countryName).forEach(elem=>{
+        if(elem.innerText.toLowerCase().includes(search.value.toLowerCase())){
+            elem.parentElement.parentElement.style.display="grid";
+
+        }
+        else{
+            elem.parentElement.parentElement.style.display="none";
+        }
+
+    })
+})
+
+toggle.addEventListener("click",()=>{
+    document.body.classList.toggle("dark")
+    moon.classList.toggle("fas")
+})
+
+
+const back=document.querySelector(".back");
+const countryModal =document.querySelector(".countryModal");
+
+
+
+function showCountryDetail(data){
+    countryModal.classList.toggle("show");
+countryModal.innerHTML=`
+<button class="back"><i class="fa-solid fa-arrow-left"></i> Back</button>
+        <div class="modal">
+            <div class="leftModal">
+                <img src="${data.flag}" alt="">
+            </div>
+            <div class="rightModal">
+                <h1>${data.name}</h1>
+                <div class="modalInfo">
+                    <div class="innerleft inner">
+                        <p><strong>Native Name:</strong>${data.nativeName}</p>
+                        <p><strong>population:</strong>${data.population}</p>
+                        <p><strong>Region:</strong>${data.region}</p>
+                        <p><strong>Sub-region:</strong>${data.subregion}</p>
+                    </div>
+                    <div class="innerright inner">
+                        <p><strong>Capital:</strong>${data.capital}</p>
+                        <p><strong>Top Level Domain:</strong>${data.topLevelDomain.map(elem=>elem)}</p>
+                        <p><strong>Currency:</strong>${data.currencies.map(elem=>elem.name)}</p>
+                        <p><strong>Languages:</strong>${data.languages.map(elem=>elem.name)}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+`
+const back=countryModal.querySelector(".back");
+
+back.addEventListener("click",()=>{
+    countryModal.classList.toggle("show");
+})
 }
